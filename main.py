a25048525e9ea1ebf098a781a90420c833cc5800
realReloadTime <max.shilcko@yandex.ru>
2024-07-05 21:49:19 +0700

    bot ver 0.2

--- main.py
+++ main.py
@@ -26,22 +26,23 @@ def start(message):
         student_button = telebot.types.KeyboardButton("Я - Студент")
         employee_button = telebot.types.KeyboardButton("Я - Сотрудник")
         markup.add(student_button, employee_button)
-        send = f"Добро пожаловать, <b>{message.from_user.first_name} {message.from_user.last_name}</b>"
+        send = (f"Добро пожаловать, <b>{message.from_user.first_name} {message.from_user.last_name}</b>!\n"
+                f"Выберите свою роль, чтобы мы могли предложить вам соответствующий функционал.")
         bot.send_message(message.chat.id, send, parse_mode='html', reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: message.text == 'Я - Студент')
+@bot.message_handler(func=lambda message: message.text == 'Я - Студент')  # выбрана роль студент
 def clicked_role_student(message):
     if not cur.execute("""SELECT role FROM profiles WHERE id=?""", (message.chat.id,)).fetchone():
         cur.execute("""INSERT INTO profiles (id, role) VALUES(?, ?)""", (message.chat.id, roles_data['student']))
         con.commit()
     markup = telebot.types.InlineKeyboardMarkup(row_width=1)
-    back_button = telebot.types.InlineKeyboardButton("Назад", callback_data='menu')
+    back_button = telebot.types.InlineKeyboardButton("Меню", callback_data='menu')
     markup.add(back_button)
     bot.send_message(message.chat.id, "Ваш профиль заполнен.", parse_mode='html', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'clear_prof')
+@bot.callback_query_handler(func=lambda call: call.data == 'clear_prof')  # забыть меня
 def clear_prof(call):
     cur.execute("""DELETE FROM profiles WHERE id=?""", (call.message.chat.id,))
     con.commit()
@@ -49,7 +50,7 @@ def clear_prof(call):
                                            "Введите /start, чтобы начать заново.", parse_mode='html')
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'menu')
+@bot.callback_query_handler(func=lambda call: call.data == 'menu')  # меню
 def callmenu_student(call):
     message = call.message
     markup = telebot.types.InlineKeyboardMarkup(row_width=1)
@@ -63,7 +64,7 @@ def callmenu_student(call):
                      parse_mode='html', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'get_template')
+@bot.callback_query_handler(func=lambda call: call.data == 'get_template')  # шаблон заявления
 def get_template(call):
     message = call.message
     markup = telebot.types.InlineKeyboardMarkup(row_width=2)
@@ -75,30 +76,95 @@ def get_template(call):
                      parse_mode='html', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'pic_doc')
+@bot.callback_query_handler(func=lambda call: call.data == 'pic_doc')  # изображение шаблона
 def get_pic_template(call):
     message = call.message
     markup = telebot.types.InlineKeyboardMarkup(row_width=1)
     file_doc = telebot.types.InlineKeyboardButton("Файл", callback_data="file_doc")
     back_button = telebot.types.InlineKeyboardButton("Назад", callback_data='menu')
     markup.add(file_doc, back_button)
-    bot.send_photo(message.chat.id, open("data/pics/matpomosh.png", 'rb'), reply_markup=markup)
+    bot.send_photo(message.chat.id, open("data/pics/matpomosh.png", 'rb'),
+                   caption="Изображение шаблона заявления", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'file_doc')
+@bot.callback_query_handler(func=lambda call: call.data == 'file_doc')  # файл шаблона
 def get_file_template(call):
     message = call.message
     markup = telebot.types.InlineKeyboardMarkup(row_width=1)
     pic_doc = telebot.types.InlineKeyboardButton("Изображение", callback_data="pic_doc")
     back_button = telebot.types.InlineKeyboardButton("Назад", callback_data='menu')
     markup.add(pic_doc, back_button)
-    bot.send_document(message.chat.id, open("data/docs/matpomosh.docx", 'rb'), reply_markup=markup)
+    bot.send_document(message.chat.id, open("data/docs/matpomosh.docx", 'rb'),
+                      caption="Файл шаблона завления", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'get_extraction')
+@bot.callback_query_handler(func=lambda call: call.data == 'get_extraction')  # что такое выписка?
 def get_extraction(call):
     message = call.message
-    bot.send_photo(message.chat.id, open("data/pics/vipiska.png", 'rb'))
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+    back_button = telebot.types.InlineKeyboardButton("Назад", callback_data='menu')
+    markup.add(back_button)
+    bot.send_photo(message.chat.id, open("data/pics/vipiska.png", 'rb'),
+                   caption="Вот так выглядит выписка платежа из СберБанка. "
+                           "Её необходимо прикреплять к заявлению, "
+                           "если оно подается на возврат средств за какую-либо покупку. "
+                           "В других банках выписка выглядит аналогично.", reply_markup=markup)
 
 
-bot.polling(none_stop=True)
+@bot.callback_query_handler(func=lambda call: call.data == 'get_cats')  # категории
+def get_cats(call):
+    message = call.message
+    bot.send_message(message.chat.id, 'BUBA')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'get_info')  # что нужно для заявления
+def get_info(call):
+    message = call.message
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+    menu_button = telebot.types.InlineKeyboardButton("Назад", callback_data='menu')
+    cats_button = telebot.types.InlineKeyboardButton("Категории матпомощи", callback_data='get_cats')
+    file_button = telebot.types.InlineKeyboardButton("Полное положение о матпомощи", callback_data='get_mat')
+    markup.add(file_button, cats_button, menu_button)
+    bot.send_message(message.chat.id, "Обучающиеся ТУСУРа имеют право на получение материальной поддержки.\n"
+                                      "❗<i>Из фонда деканата могут получать материальную помощь только бюджетники "
+                                      "очной формы обучения.\n"
+                                      "❗В деканат сдаётся готовый пакет документов: заявление → оригиналы "
+                                      "подтверждающих документов (в зависимости от категории) → копии оригиналов "
+                                      "подтверждающих документов (в зависимости от категории).</i>\n\n"
+                                      "С 1 января 2024 года отменён налог на материальную помощь, поэтому:\n"
+                                      "- ИНН не требуется;\n"
+                                      "- иностранным гражданам теперь не требуется сдавать копии паспорта "
+                                      "и миграционной карты.\n\n"
+                                      "Правила подачи материальной помощи:\n"
+                                      "1. <u>Одно заявление — один пункт положения</u>, "
+                                      "в текущий месяц можно подать только одно заявление;\n"
+                                      "2. В заявлении <u>необходимо указывать пункт(категорию), "
+                                      "по которому оно подается</u>;\n"
+                                      "3. Любые <u>чеки «действительны» только 3 месяца.</u> "
+                                      "<b>Если оплата была произведена "
+                                      "без кассового чека (online оплата), то необходимо приложить выписку из банка "
+                                      "о том, что именно Заявитель произвёл оплату со своего счёта/карты</b>\n"
+                                      "4. Стоимость билетов до дома (и обратно) будут возмещены, только если поездка "
+                                      "была в каникулярное время <u>(при перелётах самолётом сохраняйте "
+                                      "посадочные талоны).</u>\n\n"
+                                      "❗Сроки: если пакет документов сдан до 5 числа текущего месяца, то выплата "
+                                      "будет осуществлена в этом же месяце. Например, если сдали документы до "
+                                      "5 апреля -> выплата будет в апреле (в день выплаты стипендий). Если сдали "
+                                      "пакет документов после 5 апреля, то выплата материальной помощи будет "
+                                      "осуществлена уже в мае (в день выплаты стипендий).",
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'get_mat')  # файл положения
+def get_mat(call):
+    message = call.message
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+    menu_button = telebot.types.InlineKeyboardButton("Назад", callback_data='menu')
+    markup.add(menu_button)
+    bot.send_document(message.chat.id, open('data/docs/polozenie.pdf', 'rb'),
+                      caption="Положение о порядке оказания материальной поддержки нуждающимся студентам и аспирантам "
+                              "ТУСУРа, обучающимся по очной форме обучения за счет средств бюджетных ассигнований.",
+                      reply_markup=markup)
+
+
+bot.polling(none_stop=True, skip_pending=True)
